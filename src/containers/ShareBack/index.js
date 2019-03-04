1a77d1f8796b3c2f182fe9466da258a50244088b
Taiming Chen <eefozeo@gmail.com>
2019-03-04 22:08:18 +0800

    add ShareBack structure

--- src/containers/ShareBack/index.js
+++ src/containers/ShareBack/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 const ShareBack = () => (
-  <div>ShareBack</div>
+  <React.Fragment>
+    <Header />
+    <Content />
+    <Footer />
+  </React.Fragment>
 );
 
 export default ShareBack;
