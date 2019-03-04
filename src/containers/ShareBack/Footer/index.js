090a0c91c0e135d750f3349da18f01a36925b63e
Taiming Chen <eefozeo@gmail.com>
2019-03-04 23:19:32 +0800

    add footer and header

--- src/containers/ShareBack/Footer/index.js
+++ src/containers/ShareBack/Footer/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import {
   mixinWrapper,
+  MAIN_COLOR,
+  FOOTER_BACKGROUND_COLOR,
 } from 'containers/ShareBack/constants';
 
 const FooterWrapper = styled.div`
   ${mixinWrapper()}
-  border-top: 1px solid #eee;
+  border-top: 3px solid ${MAIN_COLOR};
+  background: ${FOOTER_BACKGROUND_COLOR};
+  color: white;
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Footer = () => (
-  <FooterWrapper>Footer</FooterWrapper>
+  <FooterWrapper>
+    <div>© 2019 ShareBack</div>
+  </FooterWrapper>
 );
 
 export default Footer;
--- src/containers/ShareBack/constants.js
+++ src/containers/ShareBack/constants.js
@@ -1,8 +1,9 @@
 export const NAV_BAR_HEIGHT = 50;
-export const FOOTER_HEIGHT = 50;
+export const FOOTER_HEIGHT = 80;
 
 export const mixinWrapper = () => `
   padding: 0px 20px;
 `;
 
 export const MAIN_COLOR = '#FF5722';
+export const FOOTER_BACKGROUND_COLOR = '#212121';
